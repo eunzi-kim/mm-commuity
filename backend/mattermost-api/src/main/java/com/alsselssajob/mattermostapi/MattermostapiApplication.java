ee904162434a5f7f31b254487b4a560724e0c5ee
김성규 <kskyu610@gmail.com>
2021-09-13 18:41:21 +0900

    [S05P21C103-79] [BE-kskyu610] chore: 프로젝트 준비

---
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/MattermostapiApplication.java
@@ -0,0 +1,13 @@
+package com.alsselssajob.mattermostapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattermostapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MattermostapiApplication.class, args);
+    }
+
+}
---
+++ backend/mattermost-api/src/test/java/com/alsselssajob/mattermostapi/MattermostapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsselssajob.mattermostapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattermostapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

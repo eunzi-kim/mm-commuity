cef96d55bd1f81785b3299aeeb37fbab4a00f2f7
rladmswlek <rladmswlek@naver.com>
2021-09-14 13:07:51 +0900

    [S05P21C103-102] [FE-rladmswlek] chore: 프로젝트 준비

---
+++ frontend/README.md
@@ -0,0 +1,4 @@
+## 시작하기
+npm i react-axios
+npm i react-router-dom
+npm install react-bootstrap@next bootstrap@5.1.0
---
+++ frontend/src/components/Main/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Home extends React.Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>메인화면</h1>
+      </div>
+    )
+  }
+}
+
+export default Home

b6c45b2f47e47b39a7df5159f0f4fd4c90817ef6
SOHYUN PARK <mymysuzy0627@gmail.com>
2021-09-15 09:53:22 +0900

    [BE-mymysuzy0627] docs : TIL.md 수정

---
+++ backend/auth/src/main/java/com/alsselssajob/auth/domain/Token.java
@@ -0,0 +1,16 @@
+package com.alsselssajob.auth.domain;
+
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Builder
+@NoArgsConstructor
+public class Token {
+    @Column(length = )
+
+}

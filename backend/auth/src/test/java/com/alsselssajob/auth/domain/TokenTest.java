30657c61e401d39c050676e1b47160d7a1185fa0
SOHYUN PARK <mymysuzy0627@gmail.com>
2021-09-15 15:11:00 +0900

    [S05P21C103-82] [BE-mymysuzy0627] test: Token 클래스 / Getters 테스트 생성

--- backend/auth/src/test/java/com/alsselssajob/auth/domain/TokenTest.java
+++ backend/auth/src/test/java/com/alsselssajob/auth/domain/TokenTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+//push 전에 반드시 All test 해보기!
 //ctrl + shift + T
 //메서드 이름이 길어지는 경우가 생겨서 snake case 사용
 class TokenTest {
-    Token token;
+    private Token token;
 
     // token객체 자동 생성
     @BeforeEach
@@ -35,7 +36,11 @@ class TokenTest {
     @DisplayName("Token 클래스 / Getters 테스트")
     @Test
     void getters_test() {
-
+        assertAll(
+                () -> assertThat(token.userId()).isEqualTo("asdfadfglgsdlfkj234sdf"),
+                () -> assertThat(token.token()).isEqualTo("saoggerng57kgsfhg"),
+                () -> assertThat(token.isActive()).isTrue()
+        );
 
 
     }

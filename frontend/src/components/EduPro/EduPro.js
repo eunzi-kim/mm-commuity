1b82f72af761a176633737339f76f3992b72d88c
PC2 <sjd0051@naver.com>
2021-09-16 16:47:26 +0900

    [S05P21C103-117][FE-sjd0051] feat:프로페이지

--- frontend/src/components/EduPro/EduPro.js
+++ frontend/src/components/EduPro/EduPro.js
@@ -13,16 +13,29 @@ class EduPro extends React.Component {
         return (
             <div className="pro-container">
                 <div className="pro-head">
-                    <div className="pro-title-left">
-                        왼
+                    <div className="pro-head-left">
+                        <img className="pro-logo" src={logo}/>
                     </div>
 
-                    <div className="pro-title-right">
-                        오
+                    <div className="pro-head-right">
+                        <h1>교육생 조회</h1>
                     </div>
                 </div>
-                <div className="pro-student-list-container">
-                    목록
+                
+                <div className="pro-body-container">
+                <hr />
+                    <div className="pro-body-head">
+                    <Button className="category-btn" onClick={this.onClickLogin}>게시글</Button>
+                    <Button className="category-btn" onClick={this.onClickLogin}>이모지</Button>
+                    <Button className="category-btn" onClick={this.onClickLogin}>댓  글</Button>
+                    </div>
+                    <div className="pro-body-body">
+                        <div className="pro-body-bodylist">
+                            <div>학생 1</div>
+                            <div>학생 2</div>
+                            <div>학생 3</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )

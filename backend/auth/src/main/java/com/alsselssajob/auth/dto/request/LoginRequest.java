f7ec3240ac82a84bee8a1409b1e8af3b41890c25
SOHYUN PARK <mymysuzy0627@gmail.com>
2021-09-17 10:39:15 +0900

    [S05P21C103-84] [BE-mymysuzy0627] feat: AuthController 클래스 / login API 생성

--- backend/auth/src/main/java/com/alsselssajob/auth/dto/request/LoginRequest.java
+++ backend/auth/src/main/java/com/alsselssajob/auth/dto/request/LoginRequest.java
@@ -3,7 +3,11 @@ package com.alsselssajob.auth.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+// 레이어간 통신 용도로 오고가는 객체.
+// 데이터를 오브젝트로 변환하는 객체.
 
+// 계층간 데이터 교환을 위한 객체(자바 빈스)
+// 즉, 데이터를 Service나 Controller등으로 보낼 때 사용하는 객체
 @NoArgsConstructor
 public class LoginRequest {
     //.get 표시 대신

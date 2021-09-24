19a1af837b85c638a5db7f6d5cd1f2670811e3db
김성규 <kskyu610@gmail.com>
2021-09-24 16:34:20 +0900

    [S05P21C103-145] [BE-kskyu610] test: PostController 클래스 / 생성 테스트 작성

---
+++ backend/mattermost-api/src/test/java/com/alsselssajob/mattermostapi/post/ui/PostControllerTest.java
@@ -0,0 +1,27 @@
+package com.alsselssajob.mattermostapi.post.ui;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class PostControllerTest {
+
+    private PostController postController;
+
+    @BeforeEach
+    void setUp() {
+        postController = new PostController();
+    }
+
+    @DisplayName("PostController 클래스 / 생성 테스트")
+    @Test
+    void construct_test() {
+        assertAll(
+                () -> assertThat(postController).isNotNull(),
+                () -> assertThat(postController).isExactlyInstanceOf(PostController.class)
+        );
+    }
+}

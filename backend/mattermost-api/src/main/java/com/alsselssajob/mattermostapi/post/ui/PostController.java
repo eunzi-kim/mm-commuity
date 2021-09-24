559779640e67afc32ca7e6ae35447c04e81d138d
김성규 <kskyu610@gmail.com>
2021-09-24 17:52:34 +0900

    [S05P21C103-145] [BE-kskyu610] feat: PostController 클래스 / 게시글 조회 기능 구현

--- backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/post/ui/PostController.java
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/post/ui/PostController.java
@@ -1,12 +1,15 @@
 package com.alsselssajob.mattermostapi.post.ui;
 
+import com.alsselssajob.mattermostapi.post.application.PostService;
 import lombok.NoArgsConstructor;
 import net.bis5.mattermost.client4.MattermostClient;
+import net.bis5.mattermost.model.Post;
 import net.bis5.mattermost.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.logging.Level;
 
 @Component
@@ -35,5 +38,14 @@ public class PostController {
 
     public User login() {
         return client.login(id, password).readEntity();
+    }
+
+    public List<Post> getPosts() {
+        final PostService postService = PostService.builder()
+                .client(client)
+                .user(login())
+                .build();
+
+        return postService.getPostsForToday();
     }
 }

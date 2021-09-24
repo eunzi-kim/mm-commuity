5086143f3d3070dd1fb28001c3c8e55a7dfdd47c
김성규 <kskyu610@gmail.com>
2021-09-24 11:32:27 +0900

    [S05P21C103-145] [BE-kskyu610] feat: PostService 클래스 / 팀 리스트 조회 기능 구현

--- backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/post/application/PostService.java
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/post/application/PostService.java
@@ -2,6 +2,8 @@ package com.alsselssajob.mattermostapi.post.application;
 
 import lombok.Builder;
 import net.bis5.mattermost.client4.MattermostClient;
+import net.bis5.mattermost.client4.Pager;
+import net.bis5.mattermost.model.TeamList;
 
 public class PostService {
 
@@ -12,5 +14,9 @@ public class PostService {
     public PostService(final MattermostClient client, final String etag) {
         this.client = client;
         this.etag = etag;
+    }
+
+    private TeamList getTeams() {
+        return client.getAllTeams(Pager.defaultPager(), etag).readEntity();
     }
 }

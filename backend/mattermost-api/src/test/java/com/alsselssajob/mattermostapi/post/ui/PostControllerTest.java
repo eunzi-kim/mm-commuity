07301e89a0e02d6473ccd41248af0e2e224c3f69
김성규 <kskyu610@gmail.com>
2021-09-24 17:45:32 +0900

    [S05P21C103-145] [BE-kskyu610] test: PostController 클래스 / 게시글 조회 테스트 작성

--- backend/mattermost-api/src/test/java/com/alsselssajob/mattermostapi/post/ui/PostControllerTest.java
+++ backend/mattermost-api/src/test/java/com/alsselssajob/mattermostapi/post/ui/PostControllerTest.java
@@ -1,5 +1,6 @@
 package com.alsselssajob.mattermostapi.post.ui;
 
+import net.bis5.mattermost.model.Post;
 import net.bis5.mattermost.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -35,5 +39,13 @@ class PostControllerTest {
         final User user = postController.login();
 
         assertThat(user.getId().length()).isEqualTo(26);
+    }
+
+    @DisplayName("PostController 클래스 / 게시글 조회 테스트")
+    @Test
+    void get_posts_for_user_test() {
+        final List<Post> posts = postController.getPosts();
+
+        assertThat(posts.size()).isGreaterThanOrEqualTo(0);
     }
 }

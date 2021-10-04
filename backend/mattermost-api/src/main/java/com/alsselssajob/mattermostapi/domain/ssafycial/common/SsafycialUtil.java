36e3a967a1dc7508f015a84c9ec04495529ea55f
김성규 <kskyu610@gmail.com>
2021-10-03 19:32:30 -0700

    [S05P21C103-156] [BE-kskyu610] refactor: SsafycialUtil 클래스 / 게시글을 싸피셜로 변환 메소드 반환 타입을 싸피셜로 리팩토링

--- backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/ssafycial/common/SsafycialUtil.java
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/ssafycial/common/SsafycialUtil.java
@@ -1,29 +1,22 @@
 package com.alsselssajob.mattermostapi.domain.ssafycial.common;
 
+import com.alsselssajob.mattermostapi.domain.ssafycial.domain.Ssafycial;
 import net.bis5.mattermost.model.Post;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class SsafycialUtil {
 
     private final static String HTTP_PREFIX = "http";
-    private final static String TITLE_KEY = "title";
-    private final static String LINK_KEY = "link";
-
     private final static int TITLE_INDEX = 0;
     private final static int LINK_INDEX = 1;
 
-    public static Map<String, String> parsePostToSsafycial(final Post post) {
-        final Map<String, String> ssafycial = new HashMap<>();
+    public static Ssafycial parsePostToSsafycial(final Post post) {
         final String[] titleAndLink = post.getMessage().split(HTTP_PREFIX);
 
-        ssafycial.put(TITLE_KEY, titleAndLink[TITLE_INDEX].trim());
-        ssafycial.put(LINK_KEY, HTTP_PREFIX.concat(titleAndLink[LINK_INDEX]).trim());
-
-        return ssafycial;
+        return Ssafycial.builder()
+                .title(titleAndLink[TITLE_INDEX].trim())
+                .link(HTTP_PREFIX.concat(titleAndLink[LINK_INDEX]).trim())
+                .build();
     }
 }

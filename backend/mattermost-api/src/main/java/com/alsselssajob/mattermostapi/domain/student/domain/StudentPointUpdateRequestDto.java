175ee114ff0e4876742eaf162da196e6b7ec406e
김성규 <kskyu610@gmail.com>
2021-10-04 05:33:33 -0700

    [S05P21C103-159] [BE-kskyu610] feat: StudentPointUpdateRequestDto 클래스 /  생성 기능 구현

---
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/student/domain/StudentPointUpdateRequestDto.java
@@ -0,0 +1,30 @@
+package com.alsselssajob.mattermostapi.domain.student.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class StudentPointUpdateRequestDto {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("postCountForUpdate")
+    private Integer postCountForUpdate;
+
+    @JsonProperty("reactedCountForUpdate")
+    private Integer reactedCountForUpdate;
+
+    @JsonProperty("reactingCountForUpdate")
+    private Integer reactingCountForUpdate;
+
+    @Builder
+    public StudentPointUpdateRequestDto(final String id, final Integer postCountForUpdate,
+                                        final Integer reactedCountForUpdate, final Integer reactingCountForUpdate) {
+        this.id = id;
+        this.postCountForUpdate = postCountForUpdate;
+        this.reactedCountForUpdate = reactedCountForUpdate;
+        this.reactingCountForUpdate = reactingCountForUpdate;
+    }
+}

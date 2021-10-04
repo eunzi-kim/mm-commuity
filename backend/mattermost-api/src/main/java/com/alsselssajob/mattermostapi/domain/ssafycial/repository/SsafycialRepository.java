2e68c863a524d91665563112a97525d60d7fae87
김성규 <kskyu610@gmail.com>
2021-10-03 23:52:01 -0700

    [S05P21C103-156] [BE-kskyu610] feat: SsafycialRepository 클래스 / 테이블 생성 기능 구현

--- backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/ssafycial/repository/SsafycialRepository.java
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/ssafycial/repository/SsafycialRepository.java
@@ -1,9 +1,15 @@
 package com.alsselssajob.mattermostapi.domain.ssafycial.repository;
 
+import com.alsselssajob.mattermostapi.common.vo.ColumnFamily;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.ColumnFamilyDescriptorBuilder;
+import org.apache.hadoop.hbase.client.TableDescriptorBuilder;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class SsafycialRepository {
@@ -17,4 +23,17 @@ public class SsafycialRepository {
         configuration = HBaseConfiguration.create();
         configuration.addResource(CONFIGURATION_FILE_PATH);
     }
+
+
+    private void createTableIfNotExists(final Admin admin) throws IOException {
+        if (!admin.tableExists(SSAFYCIAL_TABLE_NAME)) {
+            final TableDescriptorBuilder table = TableDescriptorBuilder.newBuilder(SSAFYCIAL_TABLE_NAME);
+
+            table.setColumnFamily(ColumnFamilyDescriptorBuilder.of(ColumnFamily.ssafycial.name()));
+            table.setColumnFamily(ColumnFamilyDescriptorBuilder.of(ColumnFamily.user.name()));
+
+            admin.createTable(table.build());
+        }
+    }
+
 }

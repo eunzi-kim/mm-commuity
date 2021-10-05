142e6dc91067137be61285173cecd2694fc639f5
mijung-oh <ks03102@naver.com>
2021-10-04 21:21:00 -0700

    [S05P21C103-144] [BE-ks03102] feat: redis post service

--- backend/post/src/main/java/com/alsselssajob/post/application/PostService.java
+++ backend/post/src/main/java/com/alsselssajob/post/application/PostService.java
@@ -1,12 +1,11 @@
 package com.alsselssajob.post.application;
 
+import com.alsselssajob.post.dto.response.ResponsePost;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +17,7 @@ import java.util.List;
 @Service
 public class PostService {
     private final static String CONF_URL = "/usr/local/hbase-2.3.6/conf/hbase-site.xml";
-    private final static TableName TABLE_NAME = TableName.valueOf("postData");
+    private final static TableName TABLE_NAME = TableName.valueOf("posts");
 
     private final Configuration configuration;
 
@@ -26,72 +25,58 @@ public class PostService {
         configuration = HBaseConfiguration.create();
         configuration.addResource(CONF_URL);
     }
-//    작성해놓은 redisPostTemplate 주입
-    @Resource(name="redisPostTemplate")
+
+    //    작성해놓은 redisPostTemplate 주입
+    @Resource(name = "redisPostTemplate")
     RedisTemplate<String, String> redisPostTemplate;
 
-//    게시글 조회
-    public void setPost() {
-//        날짜 해당하는 게시물이 redis에 있는지 확인 (key로 확인)
-//        redis에 있으면 가져와서 리턴
-//        없으면 HBase에서 가져온 뒤, redis에 넣고 리턴
-        ListOperations<String, String> stringStringListOperations = redisPostTemplate.opsForList();
-        stringStringListOperations.rightPushAll("post2", "date", "post_id", "user_id");
-        stringStringListOperations.rightPushAll("post2", "date2", "post_id2", "user_id2");
 
+    public boolean keyCheckInRedis(String key) {
+        return redisPostTemplate.hasKey(key);
     }
 
-    public void setPostWithHBase() throws IOException {
+    public List<ResponsePost> getPosts(String today_date) throws IOException {
+
+        List<ResponsePost> responsePostList = new ArrayList<>();
         final Connection connection = ConnectionFactory.createConnection();
         final Table table = connection.getTable(TABLE_NAME);
-        List<Get> gets = new ArrayList<Get>(); //Get 인스턴스들을 담을 List를 준비한다.
 
         byte[] posts = Bytes.toBytes("post");
-        byte[] dates = Bytes.toBytes("date");
-        byte[] message = Bytes.toBytes("message");
         byte[] users = Bytes.toBytes("user");
-        byte[] files = Bytes.toBytes("file");
         byte[] emojis = Bytes.toBytes("emoji");
-        byte[] row1 = Bytes.toBytes("1");
-        byte[] row2 = Bytes.toBytes("2");
+        byte[] files = Bytes.toBytes("file");
+        byte[] created_date = Bytes.toBytes("created_date");
 
-
-//        Get get1 = new Get(row1); //Get 인스턴스를 추가해준다.
-////        get1.addFamily(posts, users, files, emojis);
-//        get1.addFamily(posts);
-//        gets.add(get1);
-//        Result[] results = table.get(gets);
 
         ResultScanner results = table.getScanner(new Scan());
-        String hbaseRow1 = "1";
-        String date = "1001";
-        for(Result result:results){
-            if (new String(result.getValue(posts, dates)).equals(date)) {
 
-                for(Cell cell:result.rawCells()){
-//                    System.out.println("getFamilyArray: " + new String(cell.getFamilyArray()));
-//                    System.out.println("getQualifierArray: " + new String(cell.getQualifierArray()));
-                    System.out.println("getValueArray: " + new String(cell.getValueArray()));
-                    System.out.println("getValueArray: " + new String(cell.getValueArray()).trim());
-//                           1postdate  |<,�;0930
-                }
+        // row
+        for (Result result : results) {
 
-            }
-//            if (new String(result.getRow()).equals(hbaseRow1)){
-//
-//                for(Cell cell:result.listCells()){
-//                    System.out.println("cell: " + cell);
-//                    System.out.println("getRowArray: " + new String(cell.getRowArray()));
-//                    System.out.println("getFamilyArray: " + new String(cell.getFamilyArray()));
-//                    System.out.println("getValueArray: " + new String(cell.getValueArray()));
-////                           1postdate  |<,�;0930
-//                }
-//            }
-//                columnList.add(
-//                        new String(keyValue.getFamily()) + ":" +
-//                                new String(keyValue.getQualifier())
-//                );
+            if (new String(result.getValue(posts, created_date)).contains(today_date)) {
+                ResponsePost responsePost = ResponsePost.builder()
+                        .post_id(new String(result.getRow()))
+                        .channel_id(new String(result.getValue(posts, Bytes.toBytes("channel_id"))))
+                        .root_id(new String(result.getValue(posts, Bytes.toBytes("root_id"))))
+                        .parent_id(new String(result.getValue(posts, Bytes.toBytes("parent_id"))))
+                        .message(new String(result.getValue(posts, Bytes.toBytes("message"))))
+                        .hashTag(new String(result.getValue(posts, Bytes.toBytes("hashTag"))))
+                        .created_at(new String(result.getValue(posts, Bytes.toBytes("created_at"))))
+                        .created_date(new String(result.getValue(posts, Bytes.toBytes("created_date"))))
+                        .user_id(new String(result.getValue(users, Bytes.toBytes("user_id"))))
+                        .username(new String(result.getValue(users, Bytes.toBytes("username"))))
+                        .nickname(new String(result.getValue(users, Bytes.toBytes("nickname"))))
+                        .emoji_name(new String(result.getValue(emojis, Bytes.toBytes("name"))))
+                        .file_id(new String(result.getValue(files, Bytes.toBytes("file_id"))))
+                        .file_name(new String(result.getValue(files, Bytes.toBytes("name"))))
+                        .file_extension(new String(result.getValue(files, Bytes.toBytes("extension"))))
+                        .build();
+
+                responsePostList.add(responsePost);
             }
 
+
+        }
+        return responsePostList;
     }
 }

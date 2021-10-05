3fb1e9b2e244858bd6fcc4c55903068d6b7c7e44
김성규 <kskyu610@gmail.com>
2021-10-05 08:39:07 -0700

    [S05P21C103-161] [BE-kskyu610] feat: SsafycialController 클래스 / 싸피설 저장 기능 구현

--- backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/ssafycial/ui/SsafycialController.java
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/ssafycial/ui/SsafycialController.java
@@ -1,12 +1,15 @@
 package com.alsselssajob.mattermostapi.domain.ssafycial.ui;
 
+import com.alsselssajob.mattermostapi.domain.mattermostuser.domain.MattermostUser;
 import com.alsselssajob.mattermostapi.domain.ssafycial.applicaion.SsafycialService;
 import lombok.RequiredArgsConstructor;
 import net.bis5.mattermost.client4.MattermostClient;
+import net.bis5.mattermost.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.logging.Level;
 
 @Component
@@ -32,5 +35,19 @@ public class SsafycialController {
                 .logLevel(Level.INFO)
                 .ignoreUnknownProperties()
                 .build();
+    }
+
+    private User login() {
+        return client.login(id, password).readEntity();
+    }
+
+    public void saveSsafycialsEveryTwoWeeks() throws IOException {
+        final User user = login();
+        final MattermostUser mattermostUser = MattermostUser.builder()
+                .client(client)
+                .user(user)
+                .build();
+
+        ssafycialService.saveSsafycials(user, mattermostUser.getSsafycialsForLastTwoWeeks());
     }
 }

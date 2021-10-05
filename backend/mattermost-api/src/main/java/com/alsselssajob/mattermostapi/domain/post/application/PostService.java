94aa2aaa1e651e73269ff4653bd910f972e72156
김성규 <kskyu610@gmail.com>
2021-10-05 07:41:46 -0700

    [S05P21C103-161] [BE-kskyu610] feat: PostService 클래스 / 게시글 저장 기능 구현

--- backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/post/application/PostService.java
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/post/application/PostService.java
@@ -1,8 +1,15 @@
 package com.alsselssajob.mattermostapi.domain.post.application;
 
+import com.alsselssajob.mattermostapi.domain.mattermostuser.domain.MattermostUser;
 import com.alsselssajob.mattermostapi.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import net.bis5.mattermost.model.Post;
+import net.bis5.mattermost.model.User;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -10,5 +17,7 @@ public class PostService {
 
     private final PostRepository postRepository;
 
-
+    public void savePosts(final User user, final Map<String, List<Map<String, List<Post>>>> teams) throws IOException {
+        postRepository.savePosts(user,teams);
+    }
 }

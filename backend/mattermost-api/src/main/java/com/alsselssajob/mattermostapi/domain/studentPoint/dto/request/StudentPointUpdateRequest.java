99e5124e561163d3eb86378a5ba5b26ab89fe647
김성규 <kskyu610@gmail.com>
2021-10-04 17:23:47 -0700

    [S05P21C103-159] [BE-kskyu610] feat: StudentPointUpdateRequest 클래스 / 포인트 변수 추가

--- backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/studentPoint/dto/request/StudentPointUpdateRequest.java
+++ backend/mattermost-api/src/main/java/com/alsselssajob/mattermostapi/domain/studentPoint/dto/request/StudentPointUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.alsselssajob.mattermostapi.domain.studentPoint.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
@@ -9,41 +8,46 @@ import java.util.Objects;
 @NoArgsConstructor
 public class StudentPointUpdateRequest {
 
-    @JsonProperty("id")
+    private final static int INITIAL_POINT = 0;
+
     private String id;
-
-    @JsonProperty("postCountForUpdate")
-    private Integer postCountForUpdate;
-
-    @JsonProperty("reactedCountForUpdate")
-    private Integer reactedCountForUpdate;
-
-    @JsonProperty("reactingCountForUpdate")
-    private Integer reactingCountForUpdate;
+    private int postCountForUpdate;
+    private int reactedCountForUpdate;
+    private int reactingCountForUpdate;
+    private int point;
 
     @Builder
-    public StudentPointUpdateRequest(final String id, final Integer postCountForUpdate,
-                                     final Integer reactedCountForUpdate, final Integer reactingCountForUpdate) {
+    public StudentPointUpdateRequest(final String id, final int postCountForUpdate,
+                                     final int reactedCountForUpdate, final int reactingCountForUpdate) {
         this.id = id;
         this.postCountForUpdate = postCountForUpdate;
         this.reactedCountForUpdate = reactedCountForUpdate;
         this.reactingCountForUpdate = reactingCountForUpdate;
+        point = INITIAL_POINT;
     }
 
     public String id() {
         return id;
     }
 
-    public Integer postCountForUpdate() {
+    public int postCountForUpdate() {
         return postCountForUpdate;
     }
 
-    public Integer reactedCountForUpdate() {
+    public int reactedCountForUpdate() {
         return reactedCountForUpdate;
     }
 
-    public Integer reactingCountForUpdate() {
+    public int reactingCountForUpdate() {
         return reactingCountForUpdate;
+    }
+
+    public int point() {
+        return point;
+    }
+
+    public void updateReactingCount(int reactingCountForUpdate) {
+        this.reactingCountForUpdate = reactingCountForUpdate;
     }
 
     @Override
@@ -57,9 +61,5 @@ public class StudentPointUpdateRequest {
     @Override
     public int hashCode() {
         return Objects.hash(id);
-    }
-
-    public void updateReactingCount(int reactingCountForUpdate) {
-        this.reactingCountForUpdate = reactingCountForUpdate;
     }
 }

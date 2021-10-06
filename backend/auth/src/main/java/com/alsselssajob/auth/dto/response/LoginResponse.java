9c6c9cf8ad58489c222ba07190dd7d5f24a8b9d6
SOHYUN PARK <mymysuzy0627@gmail.com>
2021-10-06 11:16:04 +0900

    [S05P21C103-181] [BE-mymysuzy0627] feat: login시 profile image 반환 기능 추가

--- backend/auth/src/main/java/com/alsselssajob/auth/dto/response/LoginResponse.java
+++ backend/auth/src/main/java/com/alsselssajob/auth/dto/response/LoginResponse.java
@@ -14,14 +14,17 @@ public class LoginResponse {
     private String userName;
     @JsonProperty("token")
     private String token;
+    @JsonProperty("image")
+    private String image;
 
     // 생성자 자체
     @Builder
-    public LoginResponse(final String userId, final String nickName, final String userName, final String token) {
+    public LoginResponse(final String userId, final String nickName, final String userName, final String token, final String image) {
         this.userId = userId;
         this.nickName = nickName;
         this.userName = userName;
         this.token = token;
+        this.image = image;
     }
 
     public String userId() {
@@ -38,5 +41,9 @@ public class LoginResponse {
 
     public String token() {
         return token;
+    }
+
+    public String image() {
+        return image;
     }
 }

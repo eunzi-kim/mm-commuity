2854a94d098508d770bec838c50f48b31af077d1
김성규 <kskyu610@gmail.com>
2021-10-06 08:02:35 -0700

    [S05P21C103-160] [BE-kskyu610] fix: 배포 환경 설정

--- backend/mattermost-api/src/test/java/com/alsselssajob/mattermostapi/domain/post/ui/PostControllerTest.java
+++ backend/mattermost-api/src/test/java/com/alsselssajob/mattermostapi/domain/post/ui/PostControllerTest.java
@@ -1,6 +1,7 @@
 package com.alsselssajob.mattermostapi.domain.post.ui;
 
-import net.bis5.mattermost.model.Post;
+import com.alsselssajob.mattermostapi.domain.post.application.PostService;
+import com.alsselssajob.mattermostapi.domain.post.repository.PostRepository;
 import net.bis5.mattermost.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,13 +12,11 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = PostController.class)
+@ContextConfiguration(classes = {PostController.class, PostService.class, PostRepository.class})
 @TestPropertySource("classpath:/application-local.properties")
 class PostControllerTest {
 
@@ -40,14 +39,4 @@ class PostControllerTest {
 
         assertThat(user.getId().length()).isEqualTo(26);
     }
-/*
-    @DisplayName("PostController 클래스 / 게시글 조회 테스트")
-    @Test
-    void get_posts_for_user_test() {
-        final List<Post> posts = postController.getPosts();
-
-        assertThat(posts.size()).isGreaterThanOrEqualTo(0);
-    }
-    
- */
 }

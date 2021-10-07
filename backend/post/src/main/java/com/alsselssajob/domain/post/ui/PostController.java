f1a3816b4a274923752df9e8012c5e1193178a5a
mijung-oh <ks03102@naver.com>
2021-10-07 21:23:25 +0900

    [S05P21C103-192] [BE-ks03102] fix: CORS 문제 해결

--- backend/post/src/main/java/com/alsselssajob/domain/post/ui/PostController.java
+++ backend/post/src/main/java/com/alsselssajob/domain/post/ui/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin(originPatterns = "*")
 @RequiredArgsConstructor
 @RestController
 public class PostController {
--- backend/post/src/main/java/com/alsselssajob/domain/scrap/ui/ScrapController.java
+++ backend/post/src/main/java/com/alsselssajob/domain/scrap/ui/ScrapController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(originPatterns = "*")
 @RestController
 @RequiredArgsConstructor
 public class ScrapController {
